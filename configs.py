54829fa0cbd657122af5c980aa051bbfda0713da
Tech HEro <159156837+hero890@users.noreply.github.com>
2024-06-20 10:06:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,5 +7,5 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 22672907))
     API_HASH = os.environ.get("API_HASH", "0ff15ae2153bd8e03b48cb293010bc6a")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7087741098:AAFhQ6yKXdScmNcKPlutvHL1sHhUKdb5dws")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Captcha-Bot")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "URL_TO_Video_File_Bot")
     GROUP_CHAT_ID = int(os.environ.get("GROUP_CHAT_ID", -1002088051309 ))
